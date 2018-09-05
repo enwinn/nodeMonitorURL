508fbdfea3e23580904d39b25cf187ee799df3c3
Eric N. Winn <enwinn@gmail.com>
2018-09-05 00:35:57 -0400

    Cleanup indeox.js and add some images of initial scaffolding to link into README

--- index.js
+++ index.js
@@ -5,8 +5,7 @@
 
 // Dependencies
 const http = require('http');
-const { URL } = require('url');
-
+const { URL, URLSearchParams } = require('url');
 
 // The server should respond to all requests with a string
 const server = http.createServer( (req,res) => {
@@ -14,12 +13,12 @@ const server = http.createServer( (req,res) => {
   // Figuring out the user:password URL details
   // ref: https://stackoverflow.com/questions/48196706/new-url-whatwg-url-api?noredirect=1
   // ref: https://github.com/JoshuaWise/request-target
-  // ref: curl localhost:3000/fubar/showMe.aspx?checkName=check#hash
-  // ref: curl NMCcoder:MySecretPassword@localhost:3000/fubar/showMe.aspx?checkName=check#hash
   // ref: https://stackoverflow.com/questions/5951552/basic-http-authentication-in-node-js
+  // test: curl "localhost:3000/fubar/showMe.aspx?checkName=check#hash"
+  // test: curl "http://EarlFlynn:SwashBuckler@localhost:3000/abc/xyz?foo=bar&snide=sarcastic&Blackfly=eAirRiffic#hash"
 
   // Check for authorization header content
-  function checkAuth() {
+  function buildUri() {
     if (req.headers && req.headers.authorization) {
       const header=req.headers.authorization;
       const token=header.split(/\s+/).pop()||'';
@@ -30,44 +29,53 @@ const server = http.createServer( (req,res) => {
       console.log(`Auth info:
         username: ${username}
         password: ${password}`);
-      return 'http://'+username+':'+password+'@'+req.headers.host;
+        return  `http://${username}:${password}@${req.headers.host}${req.url}`;
     }
     else {
-      return 'http://'+req.headers.host;
+      return `http://${req.headers.host}${req.url}`;
     }
   }
 
   // Get the URL and parse it
-  const baseURL = checkAuth();
+  const baseURL = buildUri();
   const parsedURL = new URL(req.url, baseURL);
 
   // Get the path from the URL
   const path = parsedURL.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g,'');
 
+  // Get the query string using URLSearchParams
+  const searchParams = new URLSearchParams(parsedURL.searchParams);
+
   // Get the HTTP Method
   const method = req.method.toUpperCase();
 
+  //Iterate the search parameters to the console for sanity check.
+  for (let p of searchParams) {
+    console.log(p);
+  }
 
   // Send the response
   const parsedResponse = (`Hello, here are your request details:
 
-  url.hash......: ${parsedURL.hash}
-  url.host......: ${parsedURL.host}
-  url.hostname..: ${parsedURL.hostname}
-  url.href......: ${parsedURL.href}
-  url.origin....: ${parsedURL.origin}
-  url.password..: ${parsedURL.password}
-  url.pathname..: ${parsedURL.pathname}
-  url.port......: ${parsedURL.port}
-  url.protocol..: ${parsedURL.protocol}
-  url.search....: ${parsedURL.search}
-  url.username..: ${parsedURL.username}
-  url.toString(): ${parsedURL.toString()}
+  parsedURL.hash.........: ${parsedURL.hash}
+  parsedURL.host.........: ${parsedURL.host}
+  parsedURL.hostname.....: ${parsedURL.hostname}
+  parsedURL.href.........: ${parsedURL.href}
+  parsedURL.origin.......: ${parsedURL.origin}
+  parsedURL.password.....: ${parsedURL.password}
+  parsedURL.pathname.....: ${parsedURL.pathname}
+  parsedURL.port.........: ${parsedURL.port}
+  parsedURL.protocol.....: ${parsedURL.protocol}
+  parsedURL.search.......: ${parsedURL.search}
+  parsedURL.searchParams.: ${parsedURL.searchParams}
+  parsedURL.username.....: ${parsedURL.username}
+  parsedURL.toString()...: ${parsedURL.toString()}
 
-  path..........: ${path}
-  trimmedPath...: ${trimmedPath}
-  method........: ${method}\n`);
+  path...................: ${path}
+  trimmedPath............: ${trimmedPath}
+  method.................: ${method}
+  searchParams...........: ${searchParams.toString()}\n`);
 
   // res.end(`Hello, your URL was: ${parsedURL.toString()}\n`);
   res.end(parsedResponse);
