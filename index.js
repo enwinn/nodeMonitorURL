5dd81f6e9d917c12ab77f33b2d73336f8002f7ab
Eric N. Winn <enwinn@gmail.com>
2018-09-05 23:03:25 -0400

    Added code to route requests and return JSON

--- index.js
+++ index.js
@@ -6,9 +6,12 @@
 // Dependencies
 const http = require('http');
 const { URL, URLSearchParams } = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer( (req,res) => {
+  // req is an http.IncomingMessage, whcih is a Readable Stream
+  // res is and http.ServerResponse, which is a Writeable Stream
 
   // Figuring out the user:password URL details
   // ref: https://stackoverflow.com/questions/48196706/new-url-whatwg-url-api?noredirect=1
@@ -51,40 +54,102 @@ const server = http.createServer( (req,res) => {
   const method = req.method.toUpperCase();
 
   //Iterate the search parameters to the console for sanity check.
-  for (let p of searchParams) {
-    console.log(p);
-  }
+  // for (let p of searchParams) {
+  //   console.log(p);
+  // }
 
-  // Send the response
-  const parsedResponse = (`Hello, here are your request details:
+  // Get the headers as an object
+  const headers = req.headers;
 
-  parsedURL.hash.........: ${parsedURL.hash}
-  parsedURL.host.........: ${parsedURL.host}
-  parsedURL.hostname.....: ${parsedURL.hostname}
-  parsedURL.href.........: ${parsedURL.href}
-  parsedURL.origin.......: ${parsedURL.origin}
-  parsedURL.password.....: ${parsedURL.password}
-  parsedURL.pathname.....: ${parsedURL.pathname}
-  parsedURL.port.........: ${parsedURL.port}
-  parsedURL.protocol.....: ${parsedURL.protocol}
-  parsedURL.search.......: ${parsedURL.search}
-  parsedURL.searchParams.: ${parsedURL.searchParams}
-  parsedURL.username.....: ${parsedURL.username}
-  parsedURL.toString()...: ${parsedURL.toString()}
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on('end', () => {
+    buffer += decoder.end();
 
-  path...................: ${path}
-  trimmedPath............: ${trimmedPath}
-  method.................: ${method}
-  searchParams...........: ${searchParams.toString()}\n`);
+    // Choose the handler this request should go to.
+    // If one is not found, use the notFound handler
+    const chosenHandler = typeof(router[trimmedPath]) != 'undefined' ? router[trimmedPath] : handlers.notFount;
 
-  // res.end(`Hello, your URL was: ${parsedURL.toString()}\n`);
-  res.end(parsedResponse);
+    // Construct the data object to send to the handler
+    const data = {
+      'trimmedPath' : trimmedPath,
+      'searchParams' : searchParams,
+      'method' : method,
+      'headers' : headers,
+      'payload' : buffer
+    };
 
-  // Log the request path
+    // Route the request to the handler specified in the router
+    chosenHandler(data, (statusCode, payload) => {
+      // Use the status code called back by the handler, or default to 200
+      statusCode = typeof(statusCode) == 'number' ? statusCode : 200;
 
+      // Use the payload called back by the handler, or default to an empty object
+      payload = typeof(payload) == 'object' ? payload : {};
+
+      // Convert the payload to a string
+      const payloadString = JSON.stringify(payload);
+
+      // Return the response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // Populate the console content
+      const parsedResponse = (`Hello, here are your request details:
+
+      parsedURL.hash.........: ${parsedURL.hash}
+      parsedURL.host.........: ${parsedURL.host}
+      parsedURL.hostname.....: ${parsedURL.hostname}
+      parsedURL.href.........: ${parsedURL.href}
+      parsedURL.origin.......: ${parsedURL.origin}
+      parsedURL.password.....: ${parsedURL.password}
+      parsedURL.pathname.....: ${parsedURL.pathname}
+      parsedURL.port.........: ${parsedURL.port}
+      parsedURL.protocol.....: ${parsedURL.protocol}
+      parsedURL.search.......: ${parsedURL.search}
+      parsedURL.searchParams.: ${parsedURL.searchParams}
+      parsedURL.username.....: ${parsedURL.username}
+      parsedURL.toString()...: ${parsedURL.toString()}
+
+      path...................: ${path}
+      trimmedPath............: ${trimmedPath}
+      method.................: ${method}
+      searchParams...........: ${searchParams.toString()}
+      headers................: \n${JSON.stringify(headers,null, 2)}
+      payload................: ${buffer}\n`);
+
+      // Log the request details
+      console.log(parsedResponse+'\n');
+      console.log('Returning this response: ', statusCode, payloadString);
+
+    });
+  });
 });
 
 // Start the server and listen on port 3000
 server.listen(3000, () => {
   console.log("The server is listening on port 3000 now");
 });
+
+// Define the handlers
+const handlers = {};
+
+//Sample handler
+handlers.sample = (data, callback) => {
+  // Callback an HTTP Status Code, and a payload object
+  callback(406,{'name' : 'sample handler'});
+};
+
+// Not found handler
+handlers.notFount = (data, callback) => {
+  callback(404);
+};
+
+// Define a request router
+const router = {
+  'sample' : handlers.sample
+};
