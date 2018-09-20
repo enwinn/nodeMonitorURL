9f2c84477ef0f8d3841d44a24f7a1326a16fe7ca
Eric N. Winn <enwinn@gmail.com>
2018-09-19 22:23:16 -0400

    Fix typo in determining currentEnvironment

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ environmnets.production = {
 };
 
 // Determine whhich environment was passed as a commandline argument
-const currentEnvironment = typeof(process.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLocaleLowerCase() : '';
+const currentEnvironment = typeof(process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLocaleLowerCase() : '';
 
 // Check that the current environment is one of the environments above. If not, default to staging
 const environmentToExport = typeof(environmnets[currentEnvironment]) == 'object' ? environmnets[currentEnvironment] : environmnets.staging;
