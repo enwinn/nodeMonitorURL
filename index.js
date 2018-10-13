4d745eaf176ed5c82d3b834cfe6cc41e3b9fec1d
Eric N. Winn <enwinn@gmail.com>
2018-10-13 14:15:15 -0400

    Add library for file CRUD operations

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ const { URL, URLSearchParams } = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
 const fs = require('fs');
+const _data = require('./lib/data');
+
+
+// TESTING
+// @TODO delete this
+
+// _data.create('test', 'newFile',{'foo' : 'bar'}, (err) => {
+//   console.log('this was the error', err);
+// });
+
+// _data.read('test', 'newFile1', (err, data) => {
+//   console.log('this was the error', err, ' and this was the data', data);
+// });
+
+// _data.update('test', 'newFile',{'fizz' : 'buzz'}, (err) => {
+//   console.log('this was the error', err);
+// });
+
+// _data.delete('test', 'newFile', (err) => {
+//   console.log('this was the error', err);
+// });
 
 // Instantiating the HTTP server
 const httpServer = http.createServer( (req,res) => {
